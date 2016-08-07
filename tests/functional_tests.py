e5c5abc74219093b598e7c0c9cfcb38ca10217d1
evgeni <ecvetanov@gmail.com>
2016-08-07 20:30:55 +0300

    Adding first FT

---
+++ tests/functional_tests.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_open_page(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('Django', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
