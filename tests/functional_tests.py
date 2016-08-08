66d8805f9d05e0edbd88508cd3c5ef9de69d4e0e
evgeni <ecvetanov@gmail.com>
2016-08-08 22:08:26 +0300

    Functional test now checks for the presence of a 'Play' button

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -5,6 +5,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -13,6 +14,14 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('Baccarat', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Baccarat', header_text)
+
+        play_button = self.browser.find_element_by_id('play_button_id')
+        self.assertEqual(play_button.get_attribute('value'), 'Play')
+        play_button.click()
+
+        self.fail("finish test")
 
 if __name__ == '__main__':
     unittest.main()
