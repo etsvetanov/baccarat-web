0821f55d4161a445fb31ad1b634adc5642a84da8
Evgeni <ecvetanov@gmail.com>
2016-09-03 14:08:12 +0300

    Changing some default model values

--- base/models.py
+++ base/models.py
@@ -13,12 +13,12 @@ class Round(models.Model):
 
 
 class Options(models.Model):
-    OPTION_FIELDS = ('step', 'pair_number', 'starting_bet', 'bet_column', 'index_column', 'level_column',
+    OPTION_FIELDS = ('step', 'pairs', 'starting_bet', 'bet_column', 'index_column', 'level_column',
                      'net_column', 'partner_column', 'play_column', 'result_column', 'debt_column', 'rows')
 
     user = models.ForeignKey(User, null=True)
-    step = models.IntegerField(default=1)
-    pair_number = models.IntegerField(default=1)
+    step = models.IntegerField(default=2)
+    pair_number = models.IntegerField(default=2)
     starting_bet = models.FloatField(default=1)
     bet_column = models.BooleanField(default=True)
     index_column = models.BooleanField(default=True)
