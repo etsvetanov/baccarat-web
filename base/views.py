0c8868a2c315c48283017929e36e1781c8ca3268
Evgeni <ecvetanov@gmail.com>
2016-09-04 17:53:25 +0300

    options view properly saves the submitted data via POST using the OptionsForm

--- base/views.py
+++ base/views.py
@@ -25,68 +25,34 @@ def options(request):
         user_options = Options(user=current_user)
         user_options.save()
 
-    form = OptionsForm()
+    form = OptionsForm(instance=user_options)
 
     if request.method == 'POST':
-
-        # for attr in Options.OPTION_FIELDS:
-        #     if attr not in request.POST:
-        #         setattr(user_options, attr, False)
-        #     else:
-        #         value = request.POST[attr]
-        #         if value == 'on':
-        #             value = True
-        #         setattr(user_options, attr, value)
-
-        user_options.save()
+        submit_form = OptionsForm(request.POST, instance=user_options)
+        submit_form.save()
 
         return redirect('/')
 
     input_fields = (form['step'], form['pairs'], form['starting_bet'])
-    checkbox_fields = (form['bet_column'],
-                       form['index_column'],
-                       form['level_column'],
-                       form['net_column'],
-                       form['partner_column'],
-                       form['play_column'],
-                       form['result_column'],
-                       form['debt_column'])
+
+    column_fields = (form['bet_column'],
+                     form['index_column'],
+                     form['level_column'],
+                     form['net_column'],
+                     form['partner_column'],
+                     form['play_column'],
+                     form['result_column'],
+                     form['debt_column'])
+
+    row_fields = (form['virtual_play_rows'], form['real_player_rows'])
 
     return render(request=request,
                   template_name='base/options.html',
                   context={
                       'input_fields': input_fields,
-                      'checkbox_fields': checkbox_fields,
+                      'column_fields': column_fields,
+                      'row_fields': row_fields,
                       'form': form,
                       'input_field': IntegerField,
                       'check_box_field': BooleanField
                   })
-    # columns = ['Bet', 'Index', 'Level', 'Net', 'Partner', 'Play', 'Result', 'Debt']
-    #
-    # inputs = {'starting_bet':
-    #               {'name': 'Starting bet',
-    #                'min': 0.1,
-    #                'max': 100,
-    #                'step': 0.1,
-    #                'default': 1
-    #                },
-    #           'step':
-    #               {'name': 'Step',
-    #                'min': 2,
-    #                'max': 5,
-    #                'step': 1,
-    #                'default': 2
-    #                },
-    #           'pairs':
-    #               {'name': 'Pairs',
-    #                'min': 1,
-    #                'max': 50,
-    #                'step': 1,
-    #                'default': 1
-    #                }
-    #           }
-    #
-    # return render(request, 'base/options.html', context={
-    #     'columns': columns,
-    #     'inputs': inputs
-    # })
