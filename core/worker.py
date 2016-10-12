1ce4e8fe4bca06b8d37db6f2e0526064185201e8
Evgeni Tsvetanov <etsvet@softserveinc.com>
2016-10-12 19:01:49 +0300

    Working on the chart

--- core/worker.py
+++ core/worker.py
@@ -1,6 +1,7 @@
 # from baccarat.asgi import channel_layer
 import json
 from channels import Channel
+from asgiref.base_layer import BaseChannelLayer
 from .game import GameFactory
 from math import floor
 
@@ -14,16 +15,21 @@ def worker(game_options, channel_name):
     collector, game = factory.create()
 
     iterations = 2000
+    last_whole_percent = 0
 
     for i in range(iterations):
         game.deal()
         game.set_outcome()
-        last_whole_percent = 0
+
         current_percent = floor(((i + 1)/iterations) * 100)
         if current_percent > last_whole_percent:
+            print('current iteration:', i)
+            print('current percent:', current_percent)
+            print('last_whole_percent:', last_whole_percent)
             last_whole_percent = current_percent
 
             data = {'percentage': current_percent}
+
             Channel(channel_name).send({
                 'text': json.dumps(data)
             })
